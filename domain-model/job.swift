f4944bc836f448a7cbe5413a4de5494cfff9a6c8
yongjp94 <yongjun.park.94@gmail.com>
2017-10-17 14:41:46 -0700

    first commit

---
+++ domain-model/job.swift
@@ -0,0 +1,9 @@
+//
+//  job.swift
+//  domain-model
+//
+//  Created by Yongjun Park on 10/17/17.
+//  Copyright © 2017 Yongjun Park. All rights reserved.
+//
+
+import Foundation
---
+++ domain-model/money.swift
@@ -0,0 +1,9 @@
+//
+//  money.swift
+//  domain-model
+//
+//  Created by Yongjun Park on 10/17/17.
+//  Copyright © 2017 Yongjun Park. All rights reserved.
+//
+
+import Foundation
